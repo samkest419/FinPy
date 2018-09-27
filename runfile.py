90baa7a6fe997db46b4d3e954c5101c94d541133
rhardis <38573978+rhardis@users.noreply.github.com>
2018-09-27 09:42:53 -0400

    PYW_Email
    Emails automatically

--- runfile.py
+++ runfile.py
@@ -9,7 +9,7 @@ from finpy import *
 from strategies import macdStrat
 
 ticker = 'SPY'
-period = 'monthly'
+period = 'intraday'
 interval = '60min'
 
 
@@ -17,20 +17,14 @@ spy = pull_data(ticker,period,interval)                                      #Pu
 
 save_data(spy, 'C:\\Users\\Richard Hardis\\Documents\\GitHub\\FinPy\\','SPY')  #Save the data to csv format
 
-candlePlot(spy,period)    #Plot the candleplot
+#candlePlot(spy,period)    #Plot the candleplot
 
 spy_macd = create_macd(spy,5,25,3)
-plotMACD(spy_macd,period)
+#plotMACD(spy_macd,period)
 
-a = plotVertical(spy_macd,period)
+#plotVertical(spy_macd,period)
 
 stratout = macdStrat(spy_macd,period,interval)
 
-signal = True
-
-if signal == 'buy':
-    print('Buy SPY now!')
-elif signal == 'sell':
-    print('Sell SPY now!')
-else:
-    print('Hold SPY')
+latest_signal = stratout.iloc[-1,-1]
+emailSignal('richardphardis@gmail.com',latest_signal)
---
+++ runfile.pyw
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 26 17:03:59 2018
+
+@author: Richard Hardis
+"""
+import time
+
+from finpy import *
+from strategies import macdStrat
+
+ticker = 'SPY'
+period = 'intraday'
+interval = '1min'
+
+while True:
+    spy = pull_data(ticker,period,interval)                                      #Pull data
+    
+    save_data(spy, 'C:\\Users\\Richard Hardis\\Documents\\GitHub\\FinPy\\','SPY')  #Save the data to csv format
+    
+    #candlePlot(spy,period)    #Plot the candleplot
+    
+    spy_macd = create_macd(spy,5,25,3)
+    #plotMACD(spy_macd,period)
+    
+    #plotVertical(spy_macd,period)
+    
+    stratout = macdStrat(spy_macd,period,interval)
+
+    emailSignal('richardphardis@gmail.com',stratout)
+    time.sleep(60)  #sleep for one minute
