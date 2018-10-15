2eb09827846d40f030eda3c98501e34877239761
rhardis <38573978+rhardis@users.noreply.github.com>
2018-10-15 13:06:06 -0400

    Backtesting
    Added backtesting functions in strategies and Backtest.py.  Strategies applies the strategy to the whole dataset available and Backtest.py is a very basic test of buying and selling 100% of the shares or cash on each buy or sell signal.

--- strategies.py
+++ strategies.py
@@ -6,6 +6,7 @@ Created on Tue Sep 25 18:40:01 2018
 """
 #import pandas as pd
 import numpy as np
+import pandas as pd
 
 
 #Strategy 1: Pure MACD - no Chaikin MF
@@ -33,13 +34,6 @@ def macdStrat(df,period,interval,low_bound):
 
     return df
 
-def sell8217(df_in):
-    df = df_in.copy(deep=True)
-    
-    df['prevMACD']
-    
-    return result
-
 def oversold30min(df362,df5153,df8217):
     df362 = df362.copy(deep=True)
     df5153 = df5153.copy(deep=True)
@@ -66,6 +60,78 @@ def oversold30min(df362,df5153,df8217):
         result = 'Hold'
     
     return(result)
+
+def rollingLastSignal(list_in):
+    subsetBuyHold = [signal for signal in list_in if signal != 'Hold']
+    if (len(subsetBuyHold) > 0):
+        result = subsetBuyHold[-1]
+    else:
+        result = 'Hold'
+    return result
+
+def backTest30Min(base_df, period, interval, low_bound,window362):
+    #example: output_df = backTest30Min(spy,'intraday','30min',-0.5,10)
+    df362 = macdStrat(create_macd(base_df,3,6,2),period,interval, low_bound)
+    df5153 = macdStrat(create_macd(base_df,5,15,3),period,interval, low_bound)
+    df8217 = macdStrat(create_macd(base_df,8,21,7),period,interval, low_bound)
+    
+    #combine into one big dataframe
+    combinedDf = base_df
+    combinedDf['signals_1'] = df362['macd_trade']   #iloc -5
+    combinedDf['signals_2'] = df5153['macd_trade']  #iloc -4
+    combinedDf['signals_3'] = df8217['macd_trade']  #iloc -3
+    combinedDf = combinedDf.reset_index(drop=True)
+    
+    indices362 = np.arange(window362-1,len(combinedDf))
+    combinedDf['signals_1_window'] = 0              #iloc -2
+    combinedDf['trade_signal'] = 'Hold'             #iloc -1
+    
+    for i in indices362:
+        windowList = combinedDf.iloc[(i-9):(i+1),-5]    #Must be selecting the signals_1 column
+        combinedDf.iloc[i,-2] = rollingLastSignal(windowList)
+        
+        if ((combinedDf.iloc[i,-2] == 'Buy') and (combinedDf.iloc[i,-4] == 'Buy') and not (combinedDf.iloc[i,-3] == 'Sell')):
+            combinedDf.iloc[i,-1] = 'Buy'
+        elif ((combinedDf.iloc[i,-2] != 'Buy') and (combinedDf.iloc[i,-4] != 'Buy') and (combinedDf.iloc[i,-3] == 'Sell')):
+            combinedDf.iloc[i,-1] = 'Sell'  
+    
+    return(combinedDf)
+    
+def create_macd(df,span1,span2,span3):
+    df = df.copy(deep=True)
+    df['stock_fast_ema'] = pd.ewma(df['Close'], span=span1)
+    df['stock_slow_ema'] = pd.ewma(df['Close'], span=span2)
+    df['macd'] = df['stock_fast_ema'] - df['stock_slow_ema']
+    df['signal'] = pd.ewma(df['macd'], span=span3)
+    df['crossover'] = df['macd'] - df['signal'] # means, if this is > 0, or stock_df['Crossover'] =  stock_df['MACD'] - stock_df['Signal'] > 0, there is a buy signal                                                                     # means, if this is < 0, or stock_df['Crossover'] =  stock_df['MACD'] - stock_df['Signal'] < 0, there is a sell signal
+    return df
+
+def create_cmf(df,window):
+    df = df.copy(deep=True)
+    df['mf_multiplier'] = ((df['Close'] - df['Low']) - (df['High'] - df['Close'])) / (df['High'] - df['Low'])
+    df['mf_volume'] = df['mf_multiplier'] * df['Volume']
+    
+    df['Period CMF'] = df['mf_volume'].rolling(min_periods=1, window=window).sum() / df['Volume'].rolling(min_periods=1, window=window).sum()
+    return df
+
+def create_cmfmacd(df_fast,df_slow,span3):
+    df_fast = df_fast.copy(deep=True)    
+    df_slow = df_slow.copy(deep=True)  
+
+    df_cmf = df_fast.copy(deep=True)
+    df_cmf = df_cmf[['Close','Volume','Period CMF']]
+    df_cmf.rename(columns={'Period CMF': 'Fast MF'}, inplace=True)
+    df_cmf['Slow MF'] = df_slow['Period CMF']
+    df_cmf['CMF MACD'] = df_cmf['Fast MF'] - df_cmf['Slow MF']
+    df_cmf['CMF Signal'] = pd.ewma(df_cmf['CMF MACD'], span = span3)
+    df_cmf['CMF Crossover'] = df_cmf['CMF MACD'] - df_cmf['CMF Signal']
+    
+    #df_cmf['mf_fast'] = df_fast['Period CMF']
+    #df['mf_slow_ema'] = pd.ewma(df['Period CMF'], span=span2)
+    #df['cmf_macd'] = df['mf_fast_ema'] - df['mf_slow_ema']
+    #df['cmf_signal'] = pd.ewma(df['cmf_macd'], span=span3)
+    #df['cmf_crossover'] = df['cmf_macd'] - df['cmf_signal'] # means, if this is > 0, or stock_df['Crossover'] =  stock_df['MACD'] - stock_df['Signal'] > 0, there is a buy signal                                                                     # means, if this is < 0, or stock_df['Crossover'] =  stock_df['MACD'] - stock_df['Signal'] < 0, there is a sell signal
+    return df_cmf
     
 
 def chaikinMFStrat(df,period,interval,window):
@@ -87,3 +153,8 @@ def chaikinMFStrat(df,period,interval,window):
     df['SELL'] = df['Sell 1'] + df['Sell 2']
     
     return df
+
+class Strategy:
+    """
+    Strategy object containing instructions on how to interpret market data and produce buy and sell signals
+    """
